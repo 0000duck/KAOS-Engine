b9922713a53840c6594ca530944a1dffaa4282ef
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-01-09 21:40:41 +0000

    Adding Testing Application

---
+++ TestApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AWGL;
+
+namespace TestApplication
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            AWGL.AWGL.Main(args);
+        }
+    }
+}
