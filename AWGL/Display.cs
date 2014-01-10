46ddc9333e4c12e610fc5e3a76152983032e12e0
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-01-10 08:01:05 +0000

    Added method stubs

--- AWGL/Display.cs
+++ AWGL/Display.cs
@@ -17,9 +17,43 @@ namespace AWGL
 
         private int compiledShaderProgramID;
 
+        public Display() : base(1024, 700, new GraphicsMode(32, 24, 0, 4))
+        {
+
+        }   
+
         private void initProgram()
         {
+            compiledShaderProgramID = GL.CreateProgram();
+   
             
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+
+        private void loadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (StreamReader sr = new StreamReader("Shaders/" + filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
         }
     }
 }
