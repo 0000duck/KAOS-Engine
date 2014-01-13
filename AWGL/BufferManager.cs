cdd5cfa57dbb0493d3452cd0b91218d7376f28f4
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-01-13 12:21:01 +0000

    Added basic implementation of BufferManager.cs

--- AWGL/BufferManager.cs
+++ AWGL/BufferManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,66 @@ using System.Threading.Tasks;
 
 namespace AWGL
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class BufferManager
     {
+        #region
+        // To create a VBO:
+        // 1) Generate the buffer handles for the vertex and element buffers.
+        // 2) Bind the vertex buffer handle and upload your vertex data. 
+        //    Check that the buffer was uploaded correctly.
+        // 3) Bind the element buffer handle and upload your element data. 
+        //    Check that the buffer was uploaded correctly.
+        #endregion
+
+        private Vbo vboHandle;
+
+        private Vbo GenerateVBO<TVertex>(TVertex[] vertices,
+                                           short[] elements,
+                                           int elementSize,
+                                           int typeSize,
+                                           BufferUsageHint bufferUsageTypeGL)
+            where TVertex : struct
+        {
+            // Determine size of Buffer
+            int vbo_Size = vertices.Length * BlittableValueType.StrideOf(vertices);
+            int ebo_Size = elements.Length * sizeof(short);
+
+            //Generate Buffer ID
+            GL.GenBuffers(1, out vboHandle.VboID);
+
+            // Binds the buffer that is used next
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboHandle.VboID);
+
+            // Copy data to the VBO on the GPU.
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)vbo_Size, vertices, bufferUsageTypeGL);
+
+            CheckForErrors(vbo_Size);
+
+            GL.GenBuffers(1, out vboHandle.EboID);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, vboHandle.EboID);
+
+            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)ebo_Size, elements, bufferUsageTypeGL);
+
+            CheckForErrors(ebo_Size);
+
+            return vboHandle;
+        }
+
+        private static void CheckForErrors(int size)
+        {
+            int getBufferSize;
+            GL.GetBufferParameter(BufferTarget.ArrayBuffer, BufferParameterName.BufferSize, out getBufferSize);
+            if (getBufferSize != size)
+                throw new Exception("Data not uploaded correctly");
+        }
+
+
+        public Vbo getBufferObjects()
+        {
+            return new Vbo();
+        }
     }
 }
