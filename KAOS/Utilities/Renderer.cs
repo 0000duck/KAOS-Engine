7ee6581c87a87eaf90bf19b0b58eabfe138bee33
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-02-15 15:36:27 +0000

    moved assimp example code out of main class

--- KAOS/Utilities/Renderer.cs
+++ KAOS/Utilities/Renderer.cs
@@ -1,16 +1,26 @@
-﻿using KAOS.Managers;
+﻿using Assimp;
+using KAOS.Managers;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
 
 namespace KAOS.Utilities
 {
     public static class Renderer
     {
         internal static Matrix4 projectionMatrix, modelViewProjectionMatrix, modelViewMatrix, viewMatrix;
-        internal static Vector3 eyePosition;
-        internal static int handle_projectionMatrix, handle_modelViewProjectionMatrix, handle_modelViewMatrix, handle_eyePosition, handle_viewMatrix;
+        internal static Vector3 eyePosition, m_sceneCenter, m_sceneMin, m_sceneMax;
+        internal static int
+            handle_projectionMatrix,
+            handle_modelViewProjectionMatrix,
+            handle_modelViewMatrix, handle_eyePosition, handle_viewMatrix, m_displayList, m_texId;
+        internal static Scene m_model;
+        internal static float m_angle;
 
         public static void DrawImmediateModeVertex(Vector3d position, Color4 color, Vector2 uvs)
         {
@@ -22,7 +32,7 @@ namespace KAOS.Utilities
         public static void DrawSprite(Sprite sprite)
         {
             GL.BindTexture(TextureTarget.Texture2D, sprite.Texture.ID);
-            GL.Begin(PrimitiveType.Triangles);
+            GL.Begin(OpenTK.Graphics.OpenGL.PrimitiveType.Triangles);
             for (int i = 0; i < Sprite.VertexAmount; i++)
             {
                 DrawImmediateModeVertex(
@@ -54,12 +64,47 @@ namespace KAOS.Utilities
 
         public static void DrawWireframeVoxel(float length, float height, float width)
         {
-            
+
         }
 
         public static void DrawChunk(Chunk chunk)
         {
 
+        }
+
+        public static void DrawModel()
+        {
+            GL.Enable(EnableCap.Texture2D);
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+            GL.Enable(EnableCap.Lighting);
+            GL.Enable(EnableCap.Light0);
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.Normalize);
+            GL.FrontFace(FrontFaceDirection.Ccw);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            Matrix4 lookat = Matrix4.LookAt(0, 5, 5, 0, 0, 0, 0, 1, 0);
+            GL.LoadMatrix(ref lookat);
+
+            GL.Rotate(m_angle, 0.0f, 1.0f, 0.0f);
+
+            float tmp = m_sceneMax.X - m_sceneMin.X;
+            tmp = Math.Max(m_sceneMax.Y - m_sceneMin.Y, tmp);
+            tmp = Math.Max(m_sceneMax.Z - m_sceneMin.Z, tmp);
+            tmp = 1.0f / tmp;
+            GL.Scale(tmp * 2, tmp * 2, tmp * 2);
+
+            GL.Translate(-m_sceneCenter);
+
+            if (m_displayList == 0)
+            {
+                m_displayList = GL.GenLists(1);
+                GL.NewList(m_displayList, ListMode.Compile);
+                RecursiveRender(m_model, m_model.RootNode);
+                GL.EndList();
+            }
+
+            GL.CallList(m_displayList);
         }
 
         internal static void ToggleWireframeOn()
@@ -70,6 +115,255 @@ namespace KAOS.Utilities
         internal static void ToggleWireframeOff()
         {
             GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+        }
+
+        private static void ComputeBoundingBox()
+        {
+            m_sceneMin = new Vector3(1e10f, 1e10f, 1e10f);
+            m_sceneMax = new Vector3(-1e10f, -1e10f, -1e10f);
+            Matrix4 identity = Matrix4.Identity;
+
+            ComputeBoundingBox(m_model.RootNode, ref m_sceneMin, ref m_sceneMax, ref identity);
+
+            m_sceneCenter.X = (m_sceneMin.X + m_sceneMax.X) / 2.0f;
+            m_sceneCenter.Y = (m_sceneMin.Y + m_sceneMax.Y) / 2.0f;
+            m_sceneCenter.Z = (m_sceneMin.Z + m_sceneMax.Z) / 2.0f;
+        }
+
+        private static void ComputeBoundingBox(Node node, ref Vector3 min, ref Vector3 max, ref Matrix4 trafo)
+        {
+            Matrix4 prev = trafo;
+            trafo = Matrix4.Mult(prev, FromMatrix(node.Transform));
+
+            if (node.HasMeshes)
+            {
+                foreach (int index in node.MeshIndices)
+                {
+                    Mesh mesh = m_model.Meshes[index];
+                    for (int i = 0; i < mesh.VertexCount; i++)
+                    {
+                        Vector3 tmp = FromVector(mesh.Vertices[i]);
+                        Vector3.Transform(ref tmp, ref trafo, out tmp);
+
+                        min.X = Math.Min(min.X, tmp.X);
+                        min.Y = Math.Min(min.Y, tmp.Y);
+                        min.Z = Math.Min(min.Z, tmp.Z);
+
+                        max.X = Math.Max(max.X, tmp.X);
+                        max.Y = Math.Max(max.Y, tmp.Y);
+                        max.Z = Math.Max(max.Z, tmp.Z);
+                    }
+                }
+            }
+
+            for (int i = 0; i < node.ChildCount; i++)
+            {
+                ComputeBoundingBox(node.Children[i], ref min, ref max, ref trafo);
+            }
+            trafo = prev;
+        }
+
+        private static void RecursiveRender(Scene scene, Node node)
+        {
+            Matrix4 m = FromMatrix(node.Transform);
+            m.Transpose();
+            GL.PushMatrix();
+            GL.MultMatrix(ref m);
+
+            if (node.HasMeshes)
+            {
+                foreach (int index in node.MeshIndices)
+                {
+                    Mesh mesh = scene.Meshes[index];
+                    ApplyMaterial(scene.Materials[mesh.MaterialIndex]);
+
+                    if (mesh.HasNormals)
+                    {
+                        GL.Enable(EnableCap.Lighting);
+                    }
+                    else
+                    {
+                        GL.Disable(EnableCap.Lighting);
+                    }
+
+                    bool hasColors = mesh.HasVertexColors(0);
+                    if (hasColors)
+                    {
+                        GL.Enable(EnableCap.ColorMaterial);
+                    }
+                    else
+                    {
+                        GL.Disable(EnableCap.ColorMaterial);
+                    }
+
+                    bool hasTexCoords = mesh.HasTextureCoords(0);
+
+                    foreach (Face face in mesh.Faces)
+                    {
+                        BeginMode faceMode;
+                        switch (face.IndexCount)
+                        {
+                            case 1:
+                                faceMode = BeginMode.Points;
+                                break;
+                            case 2:
+                                faceMode = BeginMode.Lines;
+                                break;
+                            case 3:
+                                faceMode = BeginMode.Triangles;
+                                break;
+                            default:
+                                faceMode = BeginMode.Polygon;
+                                break;
+                        }
+
+                        GL.Begin(faceMode);
+                        for (int i = 0; i < face.IndexCount; i++)
+                        {
+                            int indice = face.Indices[i];
+                            if (hasColors)
+                            {
+                                Color4 vertColor = FromColor(mesh.VertexColorChannels[0][indice]);
+                            }
+                            if (mesh.HasNormals)
+                            {
+                                Vector3 normal = FromVector(mesh.Normals[indice]);
+                                GL.Normal3(normal);
+                            }
+                            if (hasTexCoords)
+                            {
+                                Vector3 uvw = FromVector(mesh.TextureCoordinateChannels[0][indice]);
+                                GL.TexCoord2(uvw.X, 1 - uvw.Y);
+                            }
+                            Vector3 pos = FromVector(mesh.Vertices[indice]);
+                            GL.Vertex3(pos);
+                        }
+                        GL.End();
+                    }
+                }
+            }
+
+            for (int i = 0; i < node.ChildCount; i++)
+            {
+                RecursiveRender(m_model, node.Children[i]);
+            }
+        }
+
+        private static void LoadTexture(String fileName)
+        {
+            fileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), fileName);
+            if (!File.Exists(fileName))
+            {
+                return;
+            }
+            Bitmap textureBitmap = new Bitmap(fileName);
+            BitmapData TextureData =
+                            textureBitmap.LockBits(
+                            new System.Drawing.Rectangle(0, 0, textureBitmap.Width, textureBitmap.Height),
+                            System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                            System.Drawing.Imaging.PixelFormat.Format24bppRgb
+                    );
+            m_texId = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, m_texId);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, textureBitmap.Width, textureBitmap.Height, 0,
+                    OpenTK.Graphics.OpenGL.PixelFormat.Bgr, PixelType.UnsignedByte, TextureData.Scan0);
+            textureBitmap.UnlockBits(TextureData);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+        }
+
+        private static void ApplyMaterial(Material mat)
+        {
+            if (mat.GetMaterialTextureCount(TextureType.Diffuse) > 0)
+            {
+                TextureSlot tex;
+                if (mat.GetMaterialTexture(TextureType.Diffuse, 0, out tex))
+                    LoadTexture(tex.FilePath);
+            }
+
+            Color4 color = new Color4(.8f, .8f, .8f, 1.0f);
+            if (mat.HasColorDiffuse)
+            {
+                // color = FromColor(mat.ColorDiffuse);
+            }
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Diffuse, color);
+
+            color = new Color4(0, 0, 0, 1.0f);
+            if (mat.HasColorSpecular)
+            {
+                color = FromColor(mat.ColorSpecular);
+            }
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Specular, color);
+
+            color = new Color4(.2f, .2f, .2f, 1.0f);
+            if (mat.HasColorAmbient)
+            {
+                color = FromColor(mat.ColorAmbient);
+            }
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Ambient, color);
+
+            color = new Color4(0, 0, 0, 1.0f);
+            if (mat.HasColorEmissive)
+            {
+                color = FromColor(mat.ColorEmissive);
+            }
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Emission, color);
+
+            float shininess = 1;
+            float strength = 1;
+            if (mat.HasShininess)
+            {
+                shininess = mat.Shininess;
+            }
+            if (mat.HasShininessStrength)
+            {
+                strength = mat.ShininessStrength;
+            }
+
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Shininess, shininess * strength);
+        }
+
+        private static Matrix4 FromMatrix(Matrix4x4 mat)
+        {
+            Matrix4 m = new Matrix4();
+            m.M11 = mat.A1;
+            m.M12 = mat.A2;
+            m.M13 = mat.A3;
+            m.M14 = mat.A4;
+            m.M21 = mat.B1;
+            m.M22 = mat.B2;
+            m.M23 = mat.B3;
+            m.M24 = mat.B4;
+            m.M31 = mat.C1;
+            m.M32 = mat.C2;
+            m.M33 = mat.C3;
+            m.M34 = mat.C4;
+            m.M41 = mat.D1;
+            m.M42 = mat.D2;
+            m.M43 = mat.D3;
+            m.M44 = mat.D4;
+            return m;
+        }
+
+        private static Vector3 FromVector(Vector3D vec)
+        {
+            Vector3 v;
+            v.X = vec.X;
+            v.Y = vec.Y;
+            v.Z = vec.Z;
+            return v;
+        }
+
+        private static Color4 FromColor(Color4D color)
+        {
+            Color4 c;
+            c.R = color.R;
+            c.G = color.G;
+            c.B = color.B;
+            c.A = color.A;
+            return c;
         }
     }
 }
