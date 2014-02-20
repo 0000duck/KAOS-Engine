de0b8f4a4e9bfd1071c19755f39439aab74b4ae5
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-02-20 00:32:40 +0000

    More detailed log, including one based on ancestry.

--- GenerateDetailedLog.sh
+++ GenerateDetailedLog.sh
@@ -1,4 +1,4 @@
 #The following command generates a report from the git log history.
-#
 
-git log --pretty=format:'<div><p><a href="http://github.com/stomppah/KAOS-Engine/commit/%H">%s</a>  -  %h</p><p>authored by %an, %ar</p></div><pre>' --shortstat -p -U1 --reverse  --decorate -- AWGL/*.cs -- KAOS/*.cs -- Game/*cs -- AWGL/*.glsl -- KAOS/*.glsl | grep -v -- Properties/ > ../DetailedLog.html
+
+git log --pretty=format:'<div><p><a href="http://github.com/stomppah/KAOS-Engine/commit/%H">%s</a>  -  %h</p><p>authored by %an, %ar</p></div><pre>' --shortstat -p -U1 --date-order --reverse --decorate -- *.cs *.glsl --dense --ancestry-path | grep -v -- Properties/ > ../DetailedLog-Ancestry-Path.html
