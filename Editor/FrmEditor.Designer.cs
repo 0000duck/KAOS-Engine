558d4e5814b86091e8ec959ac6995675fb3653c4
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-02-28 22:00:25 +0000

    Setting up basic environment.

--- Editor/FrmEditor.Designer.cs
+++ Editor/FrmEditor.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.glControl1 = new OpenTK.GLControl();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // glControl1
@@ -39,12 +40,26 @@
             this.glControl1.Size = new System.Drawing.Size(768, 576);
             this.glControl1.TabIndex = 0;
             this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl1_KeyDown);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1099, 593);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.glControl1);
             this.Name = "Editor";
             this.Text = "Editor";
@@ -55,6 +70,7 @@
         #endregion
 
         private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Editor/FrmEditor.cs
+++ Editor/FrmEditor.cs
@@ -7,14 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
 
 namespace Editor
 {
     public partial class Editor : Form
     {
+        bool glControlLoaded = false;
+        int x = 0;
+
         public Editor()
         {
             InitializeComponent();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            glControlLoaded = true;
+            Renderer.Load();
+            Renderer.SetupViewport(ref glControl1);
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!glControlLoaded) // Play nice
+                return;
+
+            Renderer.DefaultRender(ref glControl1, x);
+        }
+
+        private void glControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                x++;
+                glControl1.Invalidate();
+            }
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            Renderer.Resize(ref glControl1);
+        }
+
+    }
+
+    static class Renderer
+    {
+
+        internal static void Resize(ref OpenTK.GLControl glControl1)
+        {
+            SetupViewport(ref glControl1);
+            glControl1.Invalidate();
+        }
+
+        internal static void SetupViewport(ref OpenTK.GLControl glControl1)
+        {
+            int w = glControl1.Width;
+            int h = glControl1.Height;
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, w, 0, h, -1, 1); // Bottom-left corner pixel has coordinate (0, 0)
+            GL.Viewport(0, 0, w, h); // Use all of the glControl painting area
+        }
+
+        internal static void DefaultRender(ref OpenTK.GLControl glControl1, int x)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            GL.Translate(x, 0, 0); // position triangle according to our x variable
+
+            if (glControl1.Focused) // Simple enough :)
+                GL.Color3(Color.Yellow);
+            else
+                GL.Color3(Color.Blue);
+
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Vertex2(10, 20);
+            GL.Vertex2(100, 20);
+            GL.Vertex2(100, 50);
+            GL.End();
+
+            glControl1.SwapBuffers();
+        }
+
+        internal static void Load()
+        {
+            GL.ClearColor(Color.SkyBlue);
         }
     }
 }
