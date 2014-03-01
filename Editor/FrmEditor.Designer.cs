f112304d35501f6d648cd9d2579e4f179672615c
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-03-01 01:27:45 +0000

    Got debug text rendering.

--- Editor/FrmEditor.Designer.cs
+++ Editor/FrmEditor.Designer.cs
@@ -46,6 +46,8 @@
             this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
             this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
             this.glControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl1_KeyDown);
+            this.glControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseDown);
+            this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseMove);
             this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
             // 
             // button1
