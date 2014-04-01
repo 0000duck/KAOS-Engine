9808fbe0d81ff635a82fac23c6b8aec7cbc670dc
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-04-01 16:58:47 +0100

    small tweaks here and there, added some new textures but yet to code them into the scene.

--- KAOS/States/Skyboxstate.cs
+++ KAOS/States/Skyboxstate.cs
@@ -82,7 +82,7 @@ namespace KAOS.States
         public void Render()
         {
             Renderer.DrawSkyBox(m_textureManager, m_bufferManager.GetBuffer("SkyCube"));
-            Renderer.DrawObject(m_textureManager, m_bufferManager.GetBuffer("MengerSponge"));
+            Renderer.DrawObject(m_textureManager, m_bufferManager.GetBuffer("Sphere"));
         }
 
         public void Dispose()
