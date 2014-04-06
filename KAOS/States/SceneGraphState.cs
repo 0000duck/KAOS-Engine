8f88f4af0dd23165a4bdef147c7a1cc4f4afe299
Anthony Woodward <a.woodward6237@student.leedsmet.ac.uk>
2014-04-06 16:23:38 +0100

    Added some empty states.

---
+++ KAOS/States/SceneGraphState.cs
@@ -0,0 +1,22 @@
+﻿using KAOS.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAOS.States
+{
+    public class SceneGraphState : AbstractState, IGameObject
+    {
+        public void Update(float elapsedTime, float aspect)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
